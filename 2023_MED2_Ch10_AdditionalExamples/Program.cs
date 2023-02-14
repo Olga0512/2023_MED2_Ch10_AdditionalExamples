c26346dc171901b4d36b04d07cb7ad0dab4a4f3f
Olga0512 <ot@create.aau.dk>
2023-02-14 11:27:24 +0100

    interface example starter

--- 2023_MED2_Ch10_AdditionalExamples/Program.cs
+++ 2023_MED2_Ch10_AdditionalExamples/Program.cs
@@ -6,17 +6,56 @@ namespace _2023_MED2_Ch10_AdditionalExamples
     {
         static void Main(string[] args)
         {
+            // interface = defines a "contract" that all the classes inheriting from should follow
+
+            //             An interface declares "what a class should have"
+            //             An inheriting class defines "how it should do it"
+
+            //             benefits = security + multiple inheritance + "plug-and-play"
+
+            Rabbit rabbit = new Rabbit();
+            Hawk hawk = new Hawk();
+            Fish fish = new Fish();
+
+            rabbit.Flee();
+            hawk.Hunt();
+            fish.Flee();
+            fish.Hunt();
+
+            Console.ReadKey();
+        }
+        interface IPrey
+        {
+            void Flee();
+        }
+        interface IPredator
+        {
+            void Hunt();
+        }
+        class Rabbit : IPrey
+        {
+            public void Flee()
             {
-                int outsideVar = 20;
-                SomeFunction(out outsideVar);
-                Console.WriteLine(outsideVar);
+                Console.WriteLine("The rabbit runs away!");
             }
         }
-
-        static void SomeFunction( out int insideVar)
+        class Hawk : IPredator
         {
-            insideVar = 15;
-            insideVar = insideVar + 10;
+            public void Hunt()
+            {
+                Console.WriteLine("The hawk is searching for food!");
+            }
+        }
+        class Fish : IPrey, IPredator
+        {
+            public void Flee()
+            {
+                Console.WriteLine("The fish swims away!");
+            }
+            public void Hunt()
+            {
+                Console.WriteLine("The fish is searching for smaller fish!");
+            }
         }
     }
 }
