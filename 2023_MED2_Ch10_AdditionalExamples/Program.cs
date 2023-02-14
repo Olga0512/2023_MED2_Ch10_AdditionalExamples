8ed65384f847dc4005ec5f16f852aa0623804676
Olga0512 <ot@create.aau.dk>
2023-02-14 10:21:12 +0100

    Add project files.

---
+++ 2023_MED2_Ch10_AdditionalExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2023_MED2_Ch10_AdditionalExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
